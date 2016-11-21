8da0fd7277fa5c14b1fc6d4ea778c2331d0ac2cb
r-ruiter <rruiter@fourpoints.nl>
2016-11-21 17:51:45 +0100

    Program assign 2 changes by RR
    Functions adapted

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# Function makeCacheMatrix 
+# 1. set a matrix
+# 2. get the matrix
+# 3. set the iverse of the matrix
+# 4. get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cm <- NULL
+    set <- function(y) {
+        x <<- y
+        cm <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(solve) cm <<- solve
+    getInverse <- function() cm
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+# Function cacheSolve
+# if the inverse of the matrix is set, return the inverse
+# else inverse (solve() the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    cm <- x$getInverse()
+    if(!is.null(cm)) {
+        message("getting cached data")
+        return(cm)
+    }
+    cd <- x$get()
+    cm <- solve(cd, ...)
+    x$setInverse(cm)
+    cm
 }
